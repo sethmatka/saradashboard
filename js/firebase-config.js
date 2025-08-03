2b5655e4fb94aca6583e7f0427df6c947173d28e
sethmatka <devilbelieve1007@gmail.com>
2025-08-03 22:27:08 +0530

    Delete js directory

--- js/firebase-config.js
+++
@@ -1,9 +0,0 @@
- export const firebaseConfig = {
-  apiKey: "AIzaSyBevBXgCZ5ZE_TB2qT4nolQUZtpRU8rrdc",
-  authDomain: "sara777-86083.firebaseapp.com",
-  projectId: "sara777-86083",
-  storageBucket: "sara777-86083.firebasestorage.app",
-  messagingSenderId: "1289370023",
-  appId: "1:1289370023:web:2d92f87a1d00b63dc75e82",
-  measurementId: "G-80EZZ1Y8PR"
-};
